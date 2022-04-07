e8b651fc25a22fb2c241012fc7269f956a5bfa3b
Moritz <moritz.steigerwald@icloud.com>
2022-04-07 18:20:33 +0200

    update

--- service/Service.py
+++ service/Service.py
@@ -10,13 +10,12 @@ from fusionbase.DataService import DataService
 from fusionbase.DataStream import DataStream
 
 
-
 class Service:
 
     ZIP_CODE_PATTERN = re.compile(r'(D-)?(\d{5})')
     DE_GEO_LOOKUP = None
     CRIME_DATA_DF = None
-    REFERENCE_YEAR="2020"  
+    REFERENCE_YEAR = "2020"
     CRIME_DATA = dict()
 
     def __init__(self):
@@ -25,46 +24,52 @@ class Service:
                 self.DE_GEO_LOOKUP = json.load(fp)
                 fp.close()
 
+        if Path("./data/lookup/population_lookup.json").exists():
+            with open("./data/lookup/population_lookup.json", "r") as fp:
+                self.POP_LOOKUP = json.load(fp)
+                fp.close()
 
         # Load crime data directly on instantiation
         self.CRIME_DATA_DF = self._get_data()
-        self.CRIME_DATA_DF.sort_values(by='reference_year', ascending=False, inplace=True)
+        self.CRIME_DATA_DF.sort_values(
+            by='reference_year', ascending=False, inplace=True)
         self.CRIME_DATA_DF = self.CRIME_DATA_DF.to_dict(orient='records')
 
         for d in self.CRIME_DATA_DF:
             if d["administrative_district_key"] not in self.CRIME_DATA:
                 self.CRIME_DATA[d["administrative_district_key"]] = list()
             self.CRIME_DATA[d["administrative_district_key"]].append(d)
-            
-            
+
         self.REFERENCE_DATA = dict()
         self.CRIME_DATA_DF = pd.DataFrame(self.CRIME_DATA_DF)
-        
+
         if not Path("./data/lookup/reference_data_lookup.json").exists():
-            df = self.CRIME_DATA_DF[self.CRIME_DATA_DF["reference_year"].astype(str)=="2020"].groupby(by=["source_key"]).mean().reset_index()  
-            
-            print(df["source_key"].unique().tolist())    
-            
-            for d in df.to_dict(orient="records"):            
+            df = self.CRIME_DATA_DF[self.CRIME_DATA_DF["reference_year"].astype(
+                str) == "2020"].groupby(by=["source_key"]).mean().reset_index()
+
+            print(df["source_key"].unique().tolist())
+
+            for d in df.to_dict(orient="records"):
                 if "DEU" not in self.REFERENCE_DATA:
                     self.REFERENCE_DATA["DEU"] = dict()
-                if "MEAN" not in self.REFERENCE_DATA["DEU"]: 
-                    self.REFERENCE_DATA["DEU"]["MEAN"] = dict()  
-                # Hardcoded year for now   
-                if self.REFERENCE_YEAR not in self.REFERENCE_DATA["DEU"]["MEAN"]: 
-                    self.REFERENCE_DATA["DEU"]["MEAN"][self.REFERENCE_YEAR] = dict()       
-                
-                d["reference_year"] = int(d["reference_year"])                          
+                if "MEAN" not in self.REFERENCE_DATA["DEU"]:
+                    self.REFERENCE_DATA["DEU"]["MEAN"] = dict()
+                # Hardcoded year for now
+                if self.REFERENCE_YEAR not in self.REFERENCE_DATA["DEU"]["MEAN"]:
+                    self.REFERENCE_DATA["DEU"]["MEAN"][self.REFERENCE_YEAR] = dict(
+                    )
+
+                d["reference_year"] = int(d["reference_year"])
                 self.REFERENCE_DATA["DEU"]["MEAN"][self.REFERENCE_YEAR][d["source_key"]] = d
-                
+
             with open("./data/lookup/reference_data_lookup.json", "w") as fp:
                 json.dump(self.REFERENCE_DATA, fp)
                 fp.close()
         else:
             with open("./data/lookup/reference_data_lookup.json", "r") as fp:
                 self.REFERENCE_DATA = json.load(fp)
-                fp.close()  
-                
+                fp.close()
+
         pass
 
     @classmethod
@@ -75,7 +80,7 @@ class Service:
         zip_code = cls.ZIP_CODE_PATTERN.search(address)
         if zip_code is not None:
             try:
-                return {"data" : [{"postcode": zip_code.group(2).strip()}]}
+                return {"data": [{"postcode": zip_code.group(2).strip()}]}
             except Exception as e:
                 pass
 
@@ -84,24 +89,26 @@ class Service:
                                    connection={"base_uri": "https://api.fusionbase.com/v1"})
         location_geocoding_converter_id = 40425233
         payload = {'name': 'address', 'value': address}
-        result = data_service.invoke(key=location_geocoding_converter_id, parameters=payload)
+        result = data_service.invoke(
+            key=location_geocoding_converter_id, parameters=payload)
         return result
 
     def _get_ags_from_zip_code(self, zip_code):
-        
+
         # First, try to get from local geolookup
         if self.DE_GEO_LOOKUP is not None:
             try:
                 return str(self.DE_GEO_LOOKUP[zip_code])
             except KeyError as e:
                 pass
-        
+
         # Fallback, directly call the service
         data_service = DataService(auth={"api_key": os.getenv('FUSIONBASE_API_KEY')},
                                    connection={"base_uri": "https://api.fusionbase.com/v1"})
         zip_code_converter_id = 33387026
         payload = [{'name': 'zip_code', 'value': zip_code}]
-        result = data_service.invoke(key=zip_code_converter_id, parameters=payload)
+        result = data_service.invoke(
+            key=zip_code_converter_id, parameters=payload)
         result = result['data']
         if len(result) < 1:
             return None
@@ -117,7 +124,8 @@ class Service:
         crime_data_key = 2246748
 
         if not os.path.exists('./data/source/crime_data.parquet'):
-            last_update = data_stream.get_meta_data(crime_data_key).get('data_updated_at')
+            last_update = data_stream.get_meta_data(
+                crime_data_key).get('data_updated_at')
             df = data_stream.get_dataframe(key=crime_data_key)
             df.to_parquet('./data/source/crime_data.parquet')
             with open('./data/source/local_last_update.pickle', 'wb') as f:
@@ -127,41 +135,67 @@ class Service:
             df = pd.read_parquet('./data/source/crime_data.parquet')
 
         return df
-
-
+    
+    def get_rel_to_pop(self, value, population):
+        value_per100k = (int(value) / int(population)) * 100000
+        return round(value_per100k, 2)
+        
+        
+        
     def invoke(self, address_string, reference_year, criminal_offense_keys=[]):
         import time
         start = time.time()
-        
+
         try:
-            geo_data = self._get_location_geocoding(address_string)['data'][0]   
+            geo_data = self._get_location_geocoding(address_string)['data'][0]
             zip_code = geo_data.get('postcode')
             ags = self._get_ags_from_zip_code(zip_code)
             crime_data_json = list()
-            
+
             if isinstance(criminal_offense_keys, list) and len(criminal_offense_keys) > 0:
                 for key, co_stats in self.CRIME_DATA.items():
 
-                    if key!=ags:
+                    if key != ags:
                         continue
-                    
-                    for co_stat in co_stats:                    
+
+                    for co_stat in co_stats:
                         if co_stat["source_key"] not in criminal_offense_keys:
                             continue
-                        
-                        if reference_year == "ANY":                    
+
+                        if reference_year == "ANY":
                             crime_data_json.append(co_stat)
                         else:
                             if str(co_stat["reference_year"]) == reference_year:
-                                crime_data_json.append(co_stat)                
+                                crime_data_json.append(co_stat)
             else:
                 crime_data_json = self.CRIME_DATA[ags]
-            
-            
-            
+
             reference_data_points = list()
             for co_key in criminal_offense_keys:
-                reference_data_points.append(self.REFERENCE_DATA["DEU"]["MEAN"][str(self.REFERENCE_YEAR)][co_key])   
+                reference_data_points.append(
+                    self.REFERENCE_DATA["DEU"]["MEAN"][str(self.REFERENCE_YEAR)][co_key])
+            
+            population = self.POP_LOOKUP.get(str(ags), None)
+            population = population['population']
+            print(f'POPULATION FOR AGS {ags} IS: {population}')
+            
+                
+            if population is None:
+                crime_data_to_pop = None
+            else:
+                crime_data_to_pop = pd.DataFrame(crime_data_json)
+                integer_columns = ["number_of_cases", "frequency_count", "number_of_attempted_cases", "number_of_attempted_cases_in_percent", "threatened_with_firearm", "shot_with_firearm", "male_suspects", "female_suspects",      "number_of_non_german_suspects",
+                                   "non_german_suspects_in_percent",
+                                   "number_of_suspects",
+                                   "cleared_cases"]
+                
+                for c in integer_columns:
+                    crime_data_to_pop[f'{c}'] = crime_data_to_pop[c].apply(lambda x: self.get_rel_to_pop(x, population))
+                
+                crime_data_to_pop.rename(columns={c:f'{c}_per100k' for c in integer_columns}, inplace=True)
+                
+                    
+                crime_data_to_pop = crime_data_to_pop.to_dict(orient='records')
 
             result = {
                 'input': address_string,
@@ -171,13 +205,14 @@ class Service:
                 'state': geo_data.get('state'),
                 'administrative_district_key': ags,
                 'crime_data': crime_data_json,
+                'crime_data_per_100k': crime_data_to_pop,
+                'ags_population': population,
                 'references': [{
                     "area": "DEU",
                     "type": "MEAN",
                     "data": reference_data_points
-                }] 
+                }]
             }
-            
 
         except Exception as e:
 
@@ -190,12 +225,12 @@ class Service:
 
         end = time.time() - start
         print(end)
-        
+
         return result
 
 
 # if __name__ == '__main__':
-    
+
 #     zip_code_match = ZIP_CODE_PATTERN.search("Agnes-Pockels-Bogen 1, 80992")
 #     if zip_code_match is not None:
 #         print(zip_code_match.group(2))
